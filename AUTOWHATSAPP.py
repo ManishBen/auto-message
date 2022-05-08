b1f33d17c074bbc546b6f9ad63c6770fe90f0599
Manish Ben <69376123+ManishBen@users.noreply.github.com>
2022-05-08 20:05:55 +0530

    Add files via upload

---
+++ AUTOWHATSAPP.py
@@ -0,0 +1,8 @@
+from ctypes.wintypes import HICON
+import pyautogui
+import pywhatkit
+import time
+while True:
+   time.sleep(3)
+   pywhatkit.sendwhatmsg('+917750944643','happy birthday',1,16)
+   pyautogui.press('enter')
---
+++ autotypingtest.py
@@ -0,0 +1,7 @@
+from cv2 import detail_BlocksChannelsCompensator
+import pyautogui
+import time
+time.sleep(3)
+while True:
+ pyautogui.typewrite('Happy birthday Aman babu')
+ pyautogui.press('enter')
